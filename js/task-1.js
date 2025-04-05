7d2242d094e388fd9562ab8c5ff19bd2543fe4bb
georgiy <heorhii.kyryliuk@gmail.com>
2025-04-05 18:14:44 +0300

    tasks 1-4 done

--- js/task-1.js
+++ js/task-1.js
@@ -1,22 +1,30 @@
-// * Задача 1. Замовлення дроїдів
-// Оголоси функцію makeTransaction, яка очікує два параметри, 
-// значення яких будуть задаватися під час її виклику: 
-// • quantity— перший параметр, число, що містить кількість замовлених дроїдів 
-// • pricePerDroid — другий параметр, число, що містить вартість одного дроїда
+// * Задача 1.
+// Станція з продажу ремонтних дроїдів готова до запуску, залишилося написати програмне забезпечення
+// для відділу продажів. Оголоси функцію makeTransaction(quantity, pricePerDroid, customerCredits),
+// яка складає та повертає повідомлення про купівлю ремонтних дроїдів.
+// Вона оголошує три параметри, значення яких будуть задаватися під час її виклику:
+// quantity — кількість замовлених дроїдів
+// pricePerDroid — ціна одного дроїда
+// customerCredits — сума коштів на рахунку клієнта
+// Доповни функцію таким чином:
+// Оголоси змінну для зберігання загальної суми замовлення (загальна вартість усіх замовлених дроїдів) і задай їй вираз
+// розрахунку цієї суми.
+// Додай перевірку, чи зможе клієнт оплатити замовлення:
+// якщо сума до сплати перевищує кількість кредитів на рахунку клієнта, функція має повертати рядок "Insufficient funds!"
+// в іншому випадку функція має повертати рядок "You ordered <quantity> droids worth <totalPrice> credits!",
+// де <quantity> це кількість замовлених дроїдів, а <totalPrice> це їх загальна вартість.
 
-// Доповни код функції так, щоб вона повертала рядок з повідомленням 
-// про покупку ремонтних дроїдів: "You ordered <quantity> droids worth <totalPrice> credits!", 
-// де: • <quantity> — це кількість замовлених дроїдів 
-// • <totalPrice> — це загальна вартість замовлення, тобто вартість усіх замовлених дроїдів
-
-
-function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  const total = quantity * pricePerDroid;
+  return customerCredits >= total
+    ? `You ordered ${quantity} droids worth ${total} credits!`
+    : 'Insufficient funds!';
 }
 
 console.group('task-1');
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
 console.groupEnd();
--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,22 @@
-// * Задача 3. Ширина елемента
-// Оголоси функцію getElementWidth, яка очікує три параметри, 
-// значення яких будуть задаватися під час її виклику:
-// • content — перший параметр, ширина контенту 
-// • padding — другий параметр, значення горизонтального падінгу для кожної зі сторін 
-// • border — третій параметр, значення товщини бордера для кожної зі сторін
-// Значення всіх параметрів будуть рядками формату Npx 
-// де N — це довільне число, ціле або дробове.
-// Доповни код функції так, щоб вона повертала число — загальну ширину елемента.
-// При розрахунку загальної ширини орієнтуйся на те, що значення box-sizing дорівнює border-box.
+// * Задача 3.
+// Функція checkForSpam(message) приймає рядок (параметр message), перевіряє його на вміст заборонених слів
+// spam і sale, і повертає результат перевірки.
+// Слова в рядку параметра message можуть бути в довільному регістрі, наприклад SPAM або sAlE.
+// Доповни код функції таким чином, що:
+// Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
+// Якщо в рядку відсутні заборонені слова, функція повертає буль false
 
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + (parseFloat(padding) * 2) + (parseFloat(border) * 2);
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  return lowerCaseMessage.includes('sale') || lowerCaseMessage.includes('spam');
 }
 
 console.group('task-3');
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
 console.groupEnd();
